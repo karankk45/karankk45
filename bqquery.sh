0492e2c8f11d332bbacf7c9c679f63ffb1a1f788
karankk45 <139047512+karankk45@users.noreply.github.com>
2023-12-13 15:01:43 +0530

    Add files via upload

---
+++ bqquery.sh
@@ -0,0 +1,22 @@
+bq query --location=us --use_legacy_sql=false --use_cache=false \
+'CREATE OR REPLACE VIEW
+  bq_logs.v_querylogs AS
+SELECT
+  resource.labels.project_id,
+  protopayload_auditlog.authenticationInfo.principalEmail,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.query,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobConfiguration.query.statementType,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatus.error.message,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.startTime,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.endTime,
+  TIMESTAMP_DIFF(protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.endTime,           protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.startTime, MILLISECOND)/1000 AS run_seconds,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalProcessedBytes,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalSlotMs,
+  ARRAY(SELECT as STRUCT datasetid, tableId FROM UNNEST(protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.referencedTables)) as tables_ref,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.totalTablesProcessed,
+  protopayload_auditlog.servicedata_v1_bigquery.jobCompletedEvent.job.jobStatistics.queryOutputRowCount,
+  severity
+FROM
+  `qwiklabs-gcp-00-2e19ec982d49.bq_logs.cloudaudit_googleapis_com_data_access_*`
+ORDER BY
+  startTime'
