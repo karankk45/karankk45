200bc52eaf9324835708b8c2bad4c860135d9197
karankk45 <139047512+karankk45@users.noreply.github.com>
2024-09-10 21:32:47 +0530

    Update gsp814_1.sh

--- gsp814_1.sh
+++ gsp814_1.sh
@@ -22,6 +22,7 @@ RESET=`tput sgr0`
 
 echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
 
+export REGION="${ZONE%-*}"
 gcloud services enable datacatalog.googleapis.com
 
 echo "${YELLOW}${BOLD}Task 1. ${RESET}""${WHITE}${BOLD}Enable the Data Catalog API${RESET}" "${GREEN}${BOLD}Completed${RESET}"
@@ -35,20 +36,22 @@ cd cloudsql-sqlserver-tooling/infrastructure/terraform
 
 sed -i "s/us-central1/$REGION/g" variables.tf
 
+sed -i "s/$REGION-a/$ZONE/g" variables.tf
+
 cd ~/cloudsql-sqlserver-tooling
 bash init-db.sh
 
 gcloud iam service-accounts create sqlserver2dc-credentials \
 --display-name  "Service Account for SQL Server to Data Catalog connector" \
---project $DEVSHELL_PROJECT_ID
+--project $PROJECT_ID
 
 gcloud iam service-accounts keys create "sqlserver2dc-credentials.json" \
---iam-account "sqlserver2dc-credentials@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com"
+--iam-account "sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com"
 
-gcloud projects add-iam-policy-binding $DEVSHELL_PROJECT_ID \
---member "serviceAccount:sqlserver2dc-credentials@$DEVSHELL_PROJECT_ID.iam.gserviceaccount.com" \
+gcloud projects add-iam-policy-binding $PROJECT_ID \
+--member "serviceAccount:sqlserver2dc-credentials@$PROJECT_ID.iam.gserviceaccount.com" \
 --quiet \
---project $DEVSHELL_PROJECT_ID \
+--project $PROJECT_ID \
 --role "roles/datacatalog.admin"
 
 cd infrastructure/terraform/
@@ -62,7 +65,7 @@ cd ~/cloudsql-sqlserver-tooling
 
 docker run --rm --tty -v \
 "$PWD":/data mesmacosta/sqlserver2datacatalog:stable \
---datacatalog-project-id=$DEVSHELL_PROJECT_ID \
+--datacatalog-project-id=$PROJECT_ID \
 --datacatalog-location-id=$REGION \
 --sqlserver-host=$public_ip_address \
 --sqlserver-user=$username \
@@ -70,4 +73,3 @@ docker run --rm --tty -v \
 --sqlserver-database=$database
 
 echo "${YELLOW}${BOLD}Task 2. ${RESET}""${WHITE}${BOLD}SQL Server to Dataplex${RESET}" "${GREEN}${BOLD}Completed${RESET}"
-
